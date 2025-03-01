d3ab673fb5bed745cd788e32aa700cb2fe0d51de
scuevas1 <scuevas1@hawk.iit.edu>
2025-03-01 00:08:27 -0600

    now adding in the console.log(). this will show the output of the code in an organized manner.

--- scuevas-lab3.js
+++ scuevas-lab3.js
@@ -7,7 +7,8 @@ function minMaxAverage(numbers) {
     let max = Math.max(...numbers);
     let total = numbers.length;
     let avg = numbers.reduce((sum, num) => sum + num, 0) / total;
-    
+    console.log(`Total Numbers: ${total}, Min Value: ${min}, Max Value: ${max}, Average: ${avg.toFixed(2)}`);
+
 }
 
 //Exercise #2
