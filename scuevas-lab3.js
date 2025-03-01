4c0da58934d966dda77817f477e4eac19bb41f3c
scuevas1 <scuevas1@hawk.iit.edu>
2025-03-01 00:05:29 -0600

    not adding in the average portion. it gives the average number in the array

--- scuevas-lab3.js
+++ scuevas-lab3.js
@@ -6,7 +6,8 @@ function minMaxAverage(numbers) {
     let min = Math.min(...numbers);
     let max = Math.max(...numbers);
     let total = numbers.length;
-
+    let avg = numbers.reduce((sum, num) => sum + num, 0) / total;
+    
 }
 
 //Exercise #2
