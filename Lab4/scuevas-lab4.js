4ce532be401b78065293d8701555c9bbb19dbef8
scuevas1 <scuevas1@hawk.iit.edu>
2025-03-27 20:05:40 -0500

    testing out the main headline changes

--- Lab4/scuevas-lab4.js
+++ Lab4/scuevas-lab4.js
@@ -3,4 +3,8 @@
 
 (function () {
 
+    function updateHero(){
+        //changes the meain headline
+        document.querySelector('#hero h1').textContent = "Uplift Your Brand with Stellar Marketing";
+    }
 })();
