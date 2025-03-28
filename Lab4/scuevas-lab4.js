3d50126500ab91fb34b45b9609db40b172393060
scuevas1 <scuevas1@hawk.iit.edu>
2025-03-27 23:10:25 -0500

    testing out the image change for the background.

--- Lab4/scuevas-lab4.js
+++ Lab4/scuevas-lab4.js
@@ -9,5 +9,12 @@
         //changes the line of text below the hero headline.
         //making the words 'thrive' and 'excel' italic and bold
         document.querySelector('#hero p').innerHTML = "Utilize cutting-edge strategies from Stellar Marketing to help your business <strong><em>thrive</em></strong> and <strong><em>excel</em></strong>.";
+
+        const mainBackgroundImage = document.querySelector('section');
+        if (mainBackgroundImage) {
+            mainBackgroundImage.style.backgroundImage = "url('https://picsum.photos/id/683/1280/720')";
+        }
     }
+
+    updateHero();
 })();
