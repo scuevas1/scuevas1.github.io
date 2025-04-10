47efbe746d0f29e94cd3b5ebae64527de16cae18
scuevas1 <scuevas1@hawk.iit.edu>
2025-04-09 21:57:52 -0500

    adding in the js file with the starting code

---
+++ Lab5/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // this grabs the form and all the inputs inside of it
+    const billInput = document.getElementById("billTotal");
+    const tipSlider = document.getElementById("tipSlider");
+    const tipPercentage = document.getElementById("tipPercentage");
+    const tipAmount = document.getElementById("tipAmount");
+    const totalWithTipAndTax = document.getElementById("totalWithTipAndTax");
+
+});
