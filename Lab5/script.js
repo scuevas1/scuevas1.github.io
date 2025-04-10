832922300dcad06bf3cfbefbbfe1b932befb5290
scuevas1 <scuevas1@hawk.iit.edu>
2025-04-09 22:01:59 -0500

    adding in the function that updates everything when the user types or moves the slider

--- Lab5/script.js
+++ Lab5/script.js
@@ -6,4 +6,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const tipAmount = document.getElementById("tipAmount");
     const totalWithTipAndTax = document.getElementById("totalWithTipAndTax");
 
+    //this function updates everything when the user types or moves the slider
+    function updateValues() {
+        //this grabs the number from the bill input
+        const bill = parseFloat(billInput.value);
+        const tip = parseFloat(tipSlider.value);
+
+    }
+
 });
